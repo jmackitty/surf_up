8e7c8f1c19c172f0605c88dcfee814b71d01fed7
unknown <git@github.com:jmackitty/Testing.git>
2022-06-03 21:47:40 -0400

    Add Flask.

--- from flask import Flask.py
+++ from flask import Flask.py
@@ -1,2 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello world'
+
+
+#set FLASK_APP=app.py
+
+
+#debug=TRUE
+
+# export FLASK_DEBUG=0
